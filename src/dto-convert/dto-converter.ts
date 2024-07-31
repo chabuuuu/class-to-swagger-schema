b4c60180f6e72a004a0ddaee3b9b28faf14289a9
chabuuuu <haphuthinh332004@gmail.com>
2024-07-31 23:22:50 +0700

    version(1.0.2): + remove @PropertyDescription and @PropertyExample + remove the depend on @Expose of class-validator + add @SwaggerProperty and @SwaggerExample

--- src/dto-convert/dto-converter.ts
+++
@@ -1,8 +0,0 @@
-import { ClassConstructor, plainToInstance } from "class-transformer";
-
-export function convertToDto<DTO_TYPE>(
-  DTO: ClassConstructor<DTO_TYPE>,
-  data: any
-): DTO_TYPE {
-  return plainToInstance(DTO, data, { excludeExtraneousValues: true });
-}
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { SwaggerSchemaGenerator } from "./SwaggerSchemaGenerator";
  ** Swagger schema generator from DTO
  */
 export const swaggerSchemaGenerator = new SwaggerSchemaGenerator();
-export { PropertyDescription } from "./decorator/PropertyDescription.decorator";
-export { PropertyExample } from "./decorator/PropertyExample.decorator";
+export { SwaggerExample } from "./decorator/SwaggerExample.decorator";
+export { SwaggerProperty } from "./decorator/SwaggerProperty.decorator";
